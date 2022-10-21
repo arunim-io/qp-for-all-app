097f53d2ba766626cc7ef39fe9276bed8b3b1f11
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-10-21 12:04:02 +0600

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ import 'src/modules/settings/service.dart' show SettingsService;
 void main() async {
   // Make sure that all bindings & plugins are initialized.
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load Firebase into the application.
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
@@ -16,9 +20,6 @@ void main() async {
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
-
-  // Load Firebase into the application.
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
