ce40c5322afa2a5440b5cbe0e6dcd529125e7987
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-10-23 20:28:07 +0600

    fixes

--- lib/src/modules/subject/widgets/session_list.dart
+++ lib/src/modules/subject/widgets/session_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../utils.dart' show openUrl;
-import '../providers.dart';
+import '../providers.dart' show sessionsProvider;
 
 class SessionListWidget extends ConsumerWidget {
   const SessionListWidget({super.key, required this.qualification, required this.subjectId});
@@ -27,26 +27,25 @@ class SessionListWidget extends ConsumerWidget {
       );
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) =>
-      ref.watch(sessionsStreamProvider(subjectId)).when(
-            data: (sessions) => ListView.builder(
-              restorationId: 'sessionListView',
-              itemCount: sessions.length,
-              itemBuilder: (BuildContext context, int index) {
-                final session = sessions[index];
+  Widget build(BuildContext context, WidgetRef ref) => ref.watch(sessionsProvider(subjectId)).when(
+        data: (sessions) => ListView.builder(
+          restorationId: 'sessionListView',
+          itemCount: sessions.length,
+          itemBuilder: (BuildContext context, int index) {
+            final session = sessions[index];
 
-                return ExpansionTile(
-                  title: Text(session.name),
-                  children: [
-                    tile('Paper 1'),
-                    tile('Paper 2'),
-                  ],
-                );
-              },
-            ),
-            error: (error, stackTrace) => Center(
-              child: Text(error.toString(), style: const TextStyle(color: Colors.red)),
-            ),
-            loading: () => const Center(child: CircularProgressIndicator.adaptive()),
-          );
+            return ExpansionTile(
+              title: Text(session.name),
+              children: [
+                tile('Paper 1'),
+                tile('Paper 2'),
+              ],
+            );
+          },
+        ),
+        error: (error, stackTrace) => Center(
+          child: Text(error.toString(), style: const TextStyle(color: Colors.red)),
+        ),
+        loading: () => const Center(child: CircularProgressIndicator.adaptive()),
+      );
 }
