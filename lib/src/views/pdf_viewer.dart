c34159e2386848ae01b2f4a6b4f16ef7568d8a4e
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-11-07 11:15:19 +0600

    read pdf files

--- lib/src/views/pdf_viewer.dart
+++ lib/src/views/pdf_viewer.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/file.dart' show File;
+import 'package:flutter_cache_manager/flutter_cache_manager.dart' show DefaultCacheManager;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pdf_render/pdf_render_widgets.dart' show PdfViewer;
+
+import 'error.dart' show ErrorView;
 
 class PDFViewerView extends ConsumerWidget {
   const PDFViewerView({Key? key, this.paper, this.url}) : super(key: key);
@@ -15,7 +20,14 @@ class PDFViewerView extends ConsumerWidget {
         title: Text(paper!),
         leading: const BackButton(color: Colors.white),
       ),
-      body: Center(child: Text(url!)),
+      body: FutureBuilder<File>(
+        future: DefaultCacheManager().getSingleFile(url!),
+        builder: (context, snapshot) => snapshot.hasData
+            ? PdfViewer.openFile(snapshot.data!.path)
+            : snapshot.hasError
+                ? ErrorView(error: snapshot.error!, stackTrace: snapshot.stackTrace)
+                : const Center(child: CircularProgressIndicator.adaptive()),
+      ),
     );
   }
 }
