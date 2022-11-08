7595491a5bb0bfa710cb304f1a94217e3ca53fef
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-11-08 19:43:22 +0600

    add print option and page counter to pdf viewer

--- lib/src/views/pdf_viewer.dart
+++ lib/src/views/pdf_viewer.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/file.dart' show File;
 import 'package:flutter_cache_manager/flutter_cache_manager.dart' show DefaultCacheManager;
-import 'package:pdf_render/pdf_render_widgets.dart' show PdfViewer;
-import 'package:qp_for_all/src/models.dart';
-import 'package:qp_for_all/src/services/api.dart';
+import 'package:http/http.dart' as http;
+import 'package:pdf_render/pdf_render_widgets.dart' show PdfViewer, PdfViewerController;
+import 'package:printing/printing.dart' show Printing;
 
+import '../models.dart' show PDFType;
+import '../services/api.dart' show APIService;
 import 'error.dart' show ErrorView;
 
-class PDFViewerView extends StatelessWidget {
+class PDFViewerView extends StatefulWidget {
   const PDFViewerView({Key? key, this.paper, this.url, this.type, this.title}) : super(key: key);
 
   static const routeName = '/pdf';
@@ -16,36 +18,66 @@ class PDFViewerView extends StatelessWidget {
   final PDFType? type;
 
   @override
-  Widget build(BuildContext context) {
-    final fileType = type == PDFType.qs ? 'question_paper' : 'mark_scheme';
+  State<PDFViewerView> createState() => _PDFViewerViewState();
+}
 
+class _PDFViewerViewState extends State<PDFViewerView> {
+  final controller = PdfViewerController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final fileType = widget.type == PDFType.qs ? 'question_paper' : 'mark_scheme';
     return Scaffold(
       appBar: AppBar(
-        title: ListTile(title: Text(paper!), subtitle: Text(title!), textColor: Colors.white),
+        title: ListTile(
+            title: Text(widget.paper!), subtitle: Text(widget.title!), textColor: Colors.white),
         leading: const BackButton(color: Colors.white),
         actions: [
-          PopupMenuButton(
-            icon: const Icon(Icons.more_vert, color: Colors.white),
-            itemBuilder: (context) => [
-              const PopupMenuItem<int>(value: 0, child: Text('Download')),
-            ],
-            onSelected: (value) => value == 0
-                ? APIService().downloadPDFFile(url!, '${paper}_$fileType.pdf').then(
+          IconButton(
+            icon: const Icon(Icons.download, color: Colors.white),
+            onPressed: () =>
+                APIService().downloadPDFFile(widget.url!, '${widget.paper}_$fileType.pdf').then(
                       (value) => ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(content: Text('File has been downloaded.')),
                       ),
-                    )
-                : null,
+                    ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.print, color: Colors.white),
+            onPressed: () async {
+              final response = await http.get(Uri.parse(widget.url!));
+
+              await Printing.layoutPdf(onLayout: (_) => response.bodyBytes);
+            },
           ),
         ],
       ),
       body: FutureBuilder<File>(
-        future: DefaultCacheManager().getSingleFile(url!),
+        future: DefaultCacheManager().getSingleFile(widget.url!),
         builder: (context, snapshot) => snapshot.hasData
-            ? PdfViewer.openFile(snapshot.data!.path)
+            ? PdfViewer.openFile(
+                snapshot.data!.path,
+                viewerController: controller,
+              )
             : snapshot.hasError
                 ? ErrorView(error: snapshot.error!, stackTrace: snapshot.stackTrace)
                 : const Center(child: CircularProgressIndicator.adaptive()),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        isExtended: true,
+        child: ValueListenableBuilder(
+          valueListenable: controller,
+          builder: (context, value, child) => Text(
+            controller.isReady ? '${controller.currentPageNumber} / ${controller.pageCount}' : '-',
+          ),
+        ),
       ),
     );
   }
