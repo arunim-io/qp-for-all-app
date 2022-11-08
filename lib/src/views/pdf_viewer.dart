a8d25ce0c86bf5b0ad0c6259e6051900ba7be092
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-11-09 00:07:08 +0600

    add go to page action to pdf viewer

--- lib/src/views/pdf_viewer.dart
+++ lib/src/views/pdf_viewer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/file.dart' show File;
 import 'package:flutter_cache_manager/flutter_cache_manager.dart' show DefaultCacheManager;
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:http/http.dart' as http;
 import 'package:pdf_render/pdf_render_widgets.dart' show PdfViewer, PdfViewerController;
 import 'package:printing/printing.dart' show Printing;
@@ -9,7 +10,7 @@ import '../models.dart' show PDFType;
 import '../services/api.dart' show APIService;
 import 'error.dart' show ErrorView;
 
-class PDFViewerView extends StatefulWidget {
+class PDFViewerView extends StatefulHookWidget {
   const PDFViewerView({Key? key, this.paper, this.url, this.type, this.title}) : super(key: key);
 
   static const routeName = '/pdf';
@@ -22,17 +23,21 @@ class PDFViewerView extends StatefulWidget {
 }
 
 class _PDFViewerViewState extends State<PDFViewerView> {
-  final controller = PdfViewerController();
+  final pfdViewerController = PdfViewerController();
 
   @override
   void dispose() {
-    controller.dispose();
+    pfdViewerController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final fileType = widget.type == PDFType.qs ? 'question_paper' : 'mark_scheme';
+
+    final pageNumberController =
+        useTextEditingController(text: pfdViewerController.currentPageNumber.toString());
+
     return Scaffold(
       appBar: AppBar(
         title: ListTile(
@@ -60,22 +65,44 @@ class _PDFViewerViewState extends State<PDFViewerView> {
       ),
       body: FutureBuilder<File>(
         future: DefaultCacheManager().getSingleFile(widget.url!),
-        builder: (context, snapshot) => snapshot.hasData
+        builder: (_, snapshot) => snapshot.hasData
             ? PdfViewer.openFile(
                 snapshot.data!.path,
-                viewerController: controller,
+                viewerController: pfdViewerController,
               )
             : snapshot.hasError
                 ? ErrorView(error: snapshot.error!, stackTrace: snapshot.stackTrace)
                 : const Center(child: CircularProgressIndicator.adaptive()),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: () => showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: const Text('Go to Page Number'),
+            content: TextField(controller: pageNumberController),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () => pfdViewerController
+                    .goToPage(
+                      pageNumber: int.parse(pageNumberController.text),
+                    )
+                    .then((_) => Navigator.of(context).pop()),
+                child: const Text('Continue'),
+              ),
+            ],
+          ),
+        ),
         isExtended: true,
         child: ValueListenableBuilder(
-          valueListenable: controller,
-          builder: (context, value, child) => Text(
-            controller.isReady ? '${controller.currentPageNumber} / ${controller.pageCount}' : '-',
+          valueListenable: pfdViewerController,
+          builder: (_, value, child) => Text(
+            pfdViewerController.isReady
+                ? '${pfdViewerController.currentPageNumber} / ${pfdViewerController.pageCount}'
+                : '-',
           ),
         ),
       ),
