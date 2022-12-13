d168a69741e6a70b7ff5a290622bca5e8a598ff6
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-12-13 22:23:03 +0600

    feat: :lipstick: new about page

--- lib/src/views/about.dart
+++ lib/src/views/about.dart
@@ -1,9 +1,14 @@
+// ignore_for_file: avoid_positional_boolean_parameters
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:settings_ui/settings_ui.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../controllers/settings.dart' show SettingsController;
 
 /// A Widget that shows the information of this app.
-class AboutView extends StatelessWidget {
+class AboutView extends HookWidget {
   ///
   const AboutView({super.key, required this.settingsController});
 
@@ -14,32 +19,67 @@ class AboutView extends StatelessWidget {
   final SettingsController settingsController;
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Row(
-            children: [
-              const Text('Theme: ', textScaleFactor: 1.25),
-              DropdownButton(
-                value: settingsController.themeMode,
-                onChanged: settingsController.updateThemeMode,
-                items: const [
-                  DropdownMenuItem(
-                    value: ThemeMode.system,
-                    child: Text('System Theme'),
+  Widget build(BuildContext context) {
+    final theme = useState(settingsController.themeMode);
+    final isDarkTheme = theme.value == ThemeMode.dark;
+
+    void toggleDarkMode(bool value) {
+      theme.value = value ? ThemeMode.dark : ThemeMode.light;
+      settingsController.updateThemeMode(theme.value);
+    }
+
+    return Scaffold(
+      body: Column(
+        children: [
+          const Center(
+            heightFactor: 5,
+            child: Text('QP For All', textScaleFactor: 2),
+          ),
+          SettingsList(
+            shrinkWrap: true,
+            lightTheme: SettingsThemeData(
+              titleTextColor: Theme.of(context).primaryColorDark,
+            ),
+            darkTheme: SettingsThemeData(
+              titleTextColor: Theme.of(context).primaryColorLight,
+              settingsListBackground: Theme.of(context).backgroundColor,
+            ),
+            sections: [
+              SettingsSection(
+                title: const Text('Settings'),
+                tiles: [
+                  SettingsTile.switchTile(
+                    onToggle: toggleDarkMode,
+                    initialValue: isDarkTheme,
+                    title: Text(
+                      'Enable ${isDarkTheme ? 'light' : 'dark'} theme',
+                    ),
+                    leading: Icon(
+                      // ignore: lines_longer_than_80_chars
+                      isDarkTheme ? Icons.light_mode_rounded : Icons.dark_mode_rounded,
+                    ),
                   ),
-                  DropdownMenuItem(
-                    value: ThemeMode.light,
-                    child: Text('Light Theme'),
+                ],
+              ),
+              SettingsSection(
+                title: const Text('Some useful links'),
+                tiles: [
+                  SettingsTile(
+                    title: const Text('GitHub Repository'),
+                    onPressed: (context) =>
+                        launchUrl(Uri.parse('https://www.github.com/arunim-io/qp-for-all-app/')),
                   ),
-                  DropdownMenuItem(
-                    value: ThemeMode.dark,
-                    child: Text('Dark Theme'),
-                  )
+                  SettingsTile(
+                    title: const Text('Official Website'),
+                    onPressed: (context) =>
+                        launchUrl(Uri.parse('https://www.github.com/arunim-io/qp-for-all-app/')),
+                  ),
                 ],
               ),
             ],
           ),
-        ),
-      );
+        ],
+      ),
+    );
+  }
 }
